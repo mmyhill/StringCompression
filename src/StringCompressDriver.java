6f873151ceab2542465a3409458edc2ea60ce198
Morgan <mmyhill20@kentdenver.org>
2018-09-25 13:14:45 -0600

    Still need to test big method

---
+++ src/StringCompressDriver.java
@@ -0,0 +1,18 @@
+/*
+* StringCompressDriver
+* Purpose: The goal of StringCompressDriver is
+* Author/s: Morgan Myhill
+* 9/25/18
+* On My Honor: MM
+* Collaborators:
+*/
+public class StringCompressDriver {
+    public static void main(String[] args) {
+        System.out.println(Dictionary.disemvowel("PleeaaaaseTakeTheVowelsAEIOUut"));
+//        System.out.println((char)((int)'a' - ((int)'a' - (int)'A')));
+
+        System.out.println(Dictionary.simpCompress("aaaabbbbbhhhhhttttttrrrrrr"));//a4b5h5t6r6
+
+
+    }
+}
